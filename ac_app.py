5aefc6b67eb68b91375cee3224ee76a64c6ef806
Arham Choudhury <arham.choudhury@gmail.com>
2024-12-10 16:39:44 -0600

    set options changed

--- ac_app.py
+++ ac_app.py
@@ -7,5 +7,5 @@ st.write(x, "squared is", x * x)
 # app.py  import streamlit as st  
 # Your Streamlit app code here  
 
-# if __name__ == '__main__':
-#   st.set_option('server.enableCORS', True) 
+if __name__ == '__main__':
+  st.set_option('server.enableCORS', True) 
