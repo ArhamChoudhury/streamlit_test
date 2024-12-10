adb07dfa9b052a42d15a5e6e55395e07fd5716ca
Arham Choudhury <arham.choudhury@gmail.com>
2024-12-10 16:54:33 -0600

    edits

--- ac_app.py
+++ ac_app.py
@@ -3,9 +3,5 @@ import streamlit as st
 x = st.slider("Select a value")
 st.write(x, "squared is", x * x)
 
-
-# app.py  import streamlit as st  
-# Your Streamlit app code here  
-
 if __name__ == '__main__':
-  st.set_option('server.enableCORS', True) 
+  st.set_option('server.enableCORS', True)
